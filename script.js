18d91fcff69460a5df7df5608a090f41eb53ebe9
EugeneTsalko <tefworkmail@gmail.com>
2023-06-29 23:34:06 +0300

    feat: add basic burger menu

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello world');
+const burger = document.getElementById('burger');
+const closeBtn = document.getElementById('close');
+const nav = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
+
+const toggleBurger = () => {
+  // burger.classList.toggle('overlay_active');
+  nav.classList.toggle('nav__wrapper_active');
+  overlay.classList.toggle('overlay_active');
+};
+
+const closeBurger = (event) => {
+  if (
+    event.target.classList.contains('nav__link') ||
+    event.target.classList.contains('overlay') ||
+    event.target.classList.contains('close-btn')
+  ) {
+    nav.classList.remove('nav__wrapper_active');
+    overlay.classList.remove('overlay_active');
+  }
+};
+
+burger.addEventListener('click', toggleBurger);
+nav.addEventListener('click', closeBurger);
+overlay.addEventListener('click', closeBurger);
